389803bf9db46c0773b1e31f2219d493b1025062
Tomas Wagner <tomas.wagner@gmail.com>
2018-12-12 12:58:25 +0100

    rebuild nodegit

--- download/install.js
+++ download/install.js
@@ -1,16 +1,21 @@
 /* eslint-disable no-var */
 
 // Native
-var path = require('path')
-var fs = require('fs')
+var path = require('path');
+var fs = require('fs');
 
-var dist = path.join(__dirname, 'dist')
-var src = path.join(__dirname, 'src')
+var dist = path.join(__dirname, 'dist');
+var src = path.join(__dirname, 'src');
 
 // Don't install when developing locally
 if (fs.existsSync(src)) {
   // eslint-disable-next-line unicorn/no-process-exit
-  process.exit(0)
+  process.exit(0);
 }
 
-require(path.join(dist, 'index.js'))
+fs.copyFileSync(
+  './node_modules/nodegit/build/Release/nodegit.node',
+  './download/dist/nodegit.node'
+);
+
+require(path.join(dist, 'index.js'));
